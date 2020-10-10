5140b272b39b8a3c985fde5177558a5d9241e889
Pablo Ribalta Lorenzo <pribalta@nvidia.com>
2020-10-10 13:02:36 +0200

    Example cosine function

--- test.cpp
+++ test.cpp
@@ -1,21 +1,11 @@
 #include <iostream>
+#include <cmath>
 
 int main (){
 
-    int total_money;
-    int banknote_value;
-
-    std::cout << "Please introduce an amount of money:" << std::endl;
-
-    std::cin >> total_money;
-
-    int banknotes = 0;
-
-    for (banknotes = 0; total_money >= 50; banknotes++) {
-        total_money = total_money - 50;
+    for (int i = 0; i <= 360; i++) {
+        std::cout << "Angle : " << i << " / sine = " << cos(i) << std::endl;
     }
-
-    std::cout << "Number of banknotes: " << banknotes << std::endl;
 
     return 0;
 }
