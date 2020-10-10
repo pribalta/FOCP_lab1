d459b033d062fe8424b2966c815c247e958e322b
Pablo Ribalta Lorenzo <pribalta@nvidia.com>
2020-10-10 12:45:06 +0200

    Example while loop

--- test.cpp
+++ test.cpp
@@ -2,15 +2,20 @@
 
 int main (){
 
-    int times;
+    int total_money;
 
-    std::cout << "Please introduce the number of times you want to repeat the sentence:" << std::endl;
+    std::cout << "Please introduce an amount of money:" << std::endl;
 
-    std::cin >> times;
+    std::cin >> total_money;
 
-    for(int i = 0; i < times; i++) {
-        std::cout << "I will not talk in class" << std::endl;
+    int banknotes = 0;
+
+    while(total_money >= 50) {
+        total_money = total_money - 50;
+        banknotes = banknotes + 1;
     }
+
+    std::cout << "Number of banknotes: " << banknotes << std::endl;
 
     return 0;
 }
