b8cef585476002d0eb332042c87cdfa75fc53563
Pablo Ribalta Lorenzo <pribalta@nvidia.com>
2020-10-10 13:32:46 +0200

    Example minimum function

--- test.cpp
+++ test.cpp
@@ -3,15 +3,17 @@
 
 int main (){
 
-    std::array<int, 10> data;
+    std::array<int, 10> data = {58,4,5,6,3,8,2,9,15,7};
 
-    for(int i =0; i < 10; i++) {
-        data[i] = i;
+    int smallest = data[0];
+
+    for (int i = 0; i < 10; i++) {
+        if (data[i] < smallest) {
+            smallest = data[i];
+        }
     }
 
-    for(int i =0; i < 10; i++) {
-        std::cout << data[i] << std::endl; 
-    }
+    std::cout << "Smallest number is: " << smallest << std::endl;
 
     return 0;
 }
