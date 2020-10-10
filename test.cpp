77e6695ec8b0e25907f95d53dc01a6fd320e3a6b
Pablo Ribalta Lorenzo <pribalta@nvidia.com>
2020-10-10 11:42:54 +0200

    Input/output example

--- test.cpp
+++ test.cpp
@@ -2,7 +2,15 @@
 
 int main (){
 
-    std::cout << "Hello world!" << std::endl;
+    int my_number; // Empty variable to store input
+
+    std::cout << "Please introduce a number:" << std::endl;
+
+    std::cin >> my_number; // Read input from cin and put it in variable
+
+    std::cout << "The number you introduced is:" << std::endl;
+
+    std::cout << my_number << std::endl; // print variable using cout
 
     return 0;
-} 
+}
